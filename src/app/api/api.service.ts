027c20cf86eeecdf1af50c238bc2896e2c7c6301
mostafaadawy <mostafa_adawy@ymail.com>
2022-02-04 09:48:22 +0200

    ready fordeploying

--- src/app/api/api.service.ts
+++ src/app/api/api.service.ts
@@ -56,7 +56,6 @@ export class ApiService {
 
   async upload(endpoint: string, file: File, payload: any): Promise<any> {
     const signed_url = (await this.get(`${endpoint}/signed-url/${file.name}`)).url;
-
     const headers = new HttpHeaders({'Content-Type': file.type});
     const req = new HttpRequest( 'PUT', signed_url, file,
                                   {
